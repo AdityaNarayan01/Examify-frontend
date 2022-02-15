199158b2a86a8923a5b893842ab64fbd77a45f64
yashrdr <yashrdr11@gmail.com>
2022-02-15 09:06:45 +0530

    NewTest Single

---
+++ src/Pages/Teacher/NewTest.js
@@ -0,0 +1,417 @@
+import { useState } from 'react';
+import { Box, Button, Grid, Container, Typography, Stack, TextField, FormGroup, FormControlLabel, Checkbox ,Autocomplete,Radio,FormControl,FormLabel,RadioGroup, MenuItem, Select, InputLabel} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Page from '../../components/Page';
+import {LocalizationProvider,TimePicker,DateTimePicker,DesktopDatePicker} from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import Navbar from '../../components/student/navbar/Navbar';
+
+
+export default function NewTest() {
+    const tdate = new Date().toISOString().slice(0,-5);
+    const [test,setTest] = useState({title:"", startTime:tdate, endTime:tdate, branchspecific:false, branch:null, section:null});
+    const [questions, setQuestions] = useState([{ questiontitle: "Question", marks:"1", mcqType:'single', options: [{ option: "Option", status: true }] }]);
+
+    const branchprops = {
+        options: branch,
+        getOptionLabel: (option) => option,
+    };
+
+
+    const sectionprops = {
+        options: section,
+        getOptionLabel: (option) => option,
+    };
+
+    const mcqTypeChange = (e, i) => {
+        const list = [...questions];
+        if(e.target.value == list[i].mcqType)
+        return;
+
+        if(e.target.value == 'single'){
+            list[i].options.forEach(ele => ele.status = false);
+            list[i].options[0].status = true;
+        }
+
+        list[i].mcqType = e.target.value;
+        setQuestions(list);
+    }
+
+
+     // TITLE , START TIME , END TIME , BRANCH , SECTION
+    const handleTitleChange = (e) => {
+        setTest((prevState)=>({
+            ...prevState,
+            title:e.target.value
+        }));
+    }
+
+    const handleStartTime = (newValue) => {
+        setTest((prevState)=>({
+            ...prevState,
+            startTime:newValue
+        }));
+    };
+
+    const handleEndTime = (newValue) => {
+        setTest((prevState)=>({
+            ...prevState,
+            endTime:newValue
+        }));
+    };
+
+    const handlebranch = (e) => {
+        const {name,checked} = e.target;
+        setTest((prevState)=>({
+            ...prevState,
+            [name]:checked
+        }));
+    };
+
+    const handleSelectBranch = (e,newValue) => {
+        setTest((prevState)=>({
+            ...prevState,
+            branch:newValue
+        }));
+    };
+
+    const handleSelectSection = (e,newValue) => {
+        setTest((prevState)=>({
+            ...prevState,
+            section:newValue
+        }));
+    };
+
+    const handleAddClick = () => {
+        setQuestions([...questions, { questiontitle: "Question", marks:"1", mcqType:'single', options: [{ option: "Option", status: true }] }]);
+    }
+
+
+    const handleSubmit=()=>{
+        console.log({test,questions});
+    }
+   // END TITLE
+
+
+    const handleAddOption = (e, i) => {
+        const list = [...questions];
+        list[i].options.push({ option: "Option", status: false });
+        setQuestions(list);
+    }
+
+    const handleOptionChange = (e, i, o) => {
+        const list = [...questions];
+        list[i].options[o].status = e.target.checked;
+        setQuestions(list);
+    }
+
+    const handleInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...questions];
+        list[index][name] = value;
+        setQuestions(list);
+    }
+
+    const handleOptionInputChange = (e, i, o) => {
+        const { name, value } = e.target;
+        const list = [...questions];
+        list[i].options[o].option = value;
+        setQuestions(list);
+    }
+
+    const handleDeleteQuestion = (i) => {
+        const list = [...questions];
+        list.splice(i, 1);
+        setQuestions(list);
+    }
+
+    const handleDeleteOption = (e, i, o) => {
+        const list = [...questions];
+        list[i].options.splice(o, 1);
+        setQuestions(list);
+    }
+
+    const onRadioChange = (e, i) => {
+        const list = [...questions];
+        const prevIndex = list[i].options.findIndex(item => item.status == true);
+
+        if(prevIndex == e.target.value)
+            return;
+
+        list[i].options[prevIndex].status = false
+        list[i].options[e.target.value].status = true;
+        setQuestions(list);
+    }
+
+
+    return (
+        <Page title="Examify | Add Question">
+            <Navbar />
+            <Container maxWidth="xl" >
+
+            <Box sx={{ width: [300, 500, 700, 900], mx: "auto", mt: 10, py: 1 }}>
+
+                <Box >
+                    <TextField variant="standard" fullWidth autoComplete="off" label="Title" onChange={e => handleTitleChange(e)} 
+                        value={test.title}
+                        InputLabelProps={{ style: { fontSize: 36, textAlign: "center", paddingTop: '15px' } }}
+                        inputProps={{ style: { fontSize: 36, height: 70, padding: '5px 20px', textAlign: "center" } }} />
+                </Box>
+
+                <Box sx={{ mt:5,px:0}}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <Stack spacing={3}>
+
+                            <Grid container spacing={1} justifyContent="space-between"  >
+                                <Grid item xs={12} sm={6} >
+                                    <DateTimePicker label="Start Time" 
+                                    value={test.startTime}
+                                    onChange={handleStartTime} 
+                                    renderInput={(params) => <TextField {...params} />}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <DateTimePicker label="End Time" 
+                                    value={test.endTime}
+                                    onChange={handleEndTime} 
+                                    renderInput={(params) => <TextField {...params} />}
+                                    />
+                                </Grid>
+                            </Grid>
+
+                            <Grid container spacing={1} alignItems="center" justifyContent="center" >
+                                <Grid item xs={12} md={2} >
+                                    <FormGroup >
+                                        <FormControlLabel 
+                                        control={
+                                            <Checkbox 
+                                            name="branchspecific" 
+                                            checked={test.branchspecific} 
+                                            onChange={handlebranch} 
+                                            inputProps={{ 'aria-label': 'controlled' }}
+                                            />
+                                        } 
+                                        label="Branch" 
+                                        />
+                                    </FormGroup>
+                                </Grid>
+
+                                <Grid item xs={12} sm={6} md={5}>
+
+                                    <Autocomplete
+                                    {...branchprops}
+                                    id="branch"
+                                    disableClearable
+                                    autoComplete
+                                    disabled={!test.branchspecific}
+                                    includeInputInList
+                                    value={test.branch}
+                                    onChange={(e,newValue)=>handleSelectBranch(e,newValue)}
+                                    renderInput={(params) => (
+                                        <TextField {...params} label="Branch" variant="standard" />
+                                    )}
+                                    />
+
+                                </Grid>
+
+                                <Grid item xs={12} sm={6} md={5}>
+                                    <Autocomplete
+                                    {...sectionprops}
+                                    id="Section"
+                                    disableClearable
+                                    autoComplete
+                                    disabled={!test.branchspecific}
+                                    includeInputInList
+                                    value={test.section}
+                                    onChange={(e,newValue)=>handleSelectSection(e,newValue)}
+                                    renderInput={(params) => (
+                                        <TextField {...params} label="Section" variant="standard" />
+                                    )}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Stack>
+                    </LocalizationProvider>
+                </Box>
+
+                <Box sx={{ mt: 5, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Button variant="contained" size="large" color="info" onClick={handleSubmit}>Save</Button>
+                    <Button variant="contained" size="large" onClick={handleAddClick}>Add Question</Button>
+                </Box>
+            </Box>
+
+            <Box>
+                <Stack spacing={5} sx={{ my: 5, py: 5 }}>
+                    {questions.map((q, i) => {
+                        return (
+                            <Box key={i} sx={{ p: 5 }}>
+                                <Grid container spacing={1} alignItems="center" justifyContent="center">
+                                    <Grid item xs={12} sm={3} md={2}>
+                                        <Typography variant="h5">Question {i + 1}:</Typography>
+                                    </Grid>
+
+                                    <Grid item xs={10} sm={8} md={9}>
+                                        <TextField 
+                                        id="outlined-basic" 
+                                        variant="outlined" 
+                                        fullWidth 
+                                        autoComplete="off" 
+                                        onChange={e => handleInputChange(e, i)} 
+                                        name="questiontitle" 
+                                        multiline value={q.questiontitle} 
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={2} sm={1}>
+                                        <Button 
+                                        variant="standard" 
+                                        onClick={() => handleDeleteQuestion(i)}
+                                        >
+                                            <DeleteIcon color="error" />
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+
+                                <Grid container spacing={1} sx={{ mt: 2 }}>
+                                    <Grid item xs={12} md={2}>
+                                        <Grid container spacing={1}>
+
+                                            <Grid item xs={8} md={12}>
+                                                <Button variant="contained" size="medium" onClick={e => handleAddOption(e, i)}>Add Option</Button>
+                                            </Grid>
+
+                                            <Grid item xs={4} md={12}>
+                                            <FormControl fullWidth>
+                                                <InputLabel id="demo-simple-select-label">MCQ type</InputLabel>
+                                                    <Select
+                                                    labelId="demo-simple-select-label"
+                                                    id="demo-simple-select"
+                                                    value={q.mcqType}
+                                                    label="MCQ Type"
+                                                    onChange={(e) =>  mcqTypeChange(e, i)}
+                                                    >
+                                                        <MenuItem value='single'>Single</MenuItem>
+                                                        <MenuItem value='multiple'>Multiple</MenuItem>
+                                                    </Select>
+                                            </FormControl>
+                                            </Grid>
+
+                                            <Grid item xs={4} md={12}>
+                                                <TextField
+                                                size="small"
+                                                name="marks"
+                                                label="Marks"
+                                                type="number"
+                                                variant="standard"
+                                                value={q.marks}
+                                                onWheel={(e) => e.target.blur()}
+                                                onChange={(e) => handleInputChange(e,i)}
+                                                style = {{width: 100}}
+                                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*',style:{textAlign: "center"}}}
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+
+                                <Grid item xs={12} md={10} >
+                                    {q.mcqType == 'multiple' && 
+                                        <FormGroup >
+                                        {q.options.map((op, o) => {
+                                            return (
+                                                <Grid container spacing={1} alignItems="center" sx={{ my: 0.5 }} key={o} >
+                                                    <Grid item xs={10}  >
+                                                    <FormControl>
+                                                    <FormControlLabel 
+                                                    control={
+                                                        <Checkbox 
+                                                        checked={op.status} 
+                                                        onChange={e => handleOptionChange(e, i, o)} 
+                                                        />
+                                                    } 
+                                                    label={
+                                                        <TextField 
+                                                        id="outlined-basic" 
+                                                        variant="outlined" 
+                                                        autoComplete="off" 
+                                                        name="option" 
+                                                        multiline size="small" 
+                                                        color="info" 
+                                                        onChange={e => handleOptionInputChange(e, i, o)} 
+                                                        value={op.option} 
+                                                        sx={{ width: [300, 390, 500, 725] }} 
+                                                        />
+                                                    } 
+                                                    />
+                                                    </FormControl>
+                                                </Grid>
+                                                <Grid item xs={2}>
+                                                    <Button 
+                                                    variant="standard" 
+                                                    onClick={(e) => handleDeleteOption(e, i, o)}>
+                                                        <DeleteIcon color="error" fontSize="small" />
+                                                    </Button>
+                                                </Grid>
+                                            </Grid>
+                                            );
+                                        })}
+                                    </FormGroup>    
+                                    }
+                                
+                                {q.mcqType == 'single' && 
+                                    <RadioGroup
+                                    className="noselect"
+                                    aria-labelledby="demo-radio-buttons-group-label"
+                                    name="checked"
+                                    value={q.options.findIndex(item => item.status == true)}
+                                    onChange={(e) => onRadioChange(e, i)}
+                                    >
+                                    {q.options.map((op, o) =>  (
+                                        <FormControlLabel 
+                                        key={o} 
+                                        value={o} 
+                                        control={<Radio />} 
+                                        label={
+                                            <TextField 
+                                                id="outlined-basic" 
+                                                variant="outlined" 
+                                                autoComplete="off" 
+                                                name="option" 
+                                                multiline size="small" 
+                                                color="info" 
+                                                onChange={e => handleOptionInputChange(e, i, o)} 
+                                                value={op.option} 
+                                                sx={{ width: [300, 390, 500, 725] }} 
+                                                />
+                                        }/>
+                                        ))}
+                                    </RadioGroup>
+                                }
+
+                            </Grid>
+                        </Grid>
+                    </Box>
+                    );
+                })}
+            </Stack>
+            </Box>
+        </Container>
+    </Page>
+    );
+}
+
+
+const branch=[
+   "CSE",
+   "MECH",
+   "ESE"
+]
+
+const section=[
+   "A1",
+   "A2",
+   "B1",
+   "B2",
+   "C1",
+   "C2",
+   "D1",
+]
